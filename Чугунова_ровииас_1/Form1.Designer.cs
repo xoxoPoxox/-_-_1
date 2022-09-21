f0924ed9316f3f2c467d0b8f6bca133b6939b12d
Лина <@LAPTOP-A1I4MCDA>
2022-09-21 22:35:24 +0300

    Добавьте файлы проекта.

---
+++ Чугунова_ровииас_1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Чугунова_ровииас_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.shit1 = new System.Windows.Forms.NumericUpDown();
+            this.shit2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shit2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(30, 37);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(456, 378);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(530, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Фото";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(681, 37);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(456, 378);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            this.imageBox2.Click += new System.EventHandler(this.imageBox2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(530, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Камера";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(530, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Видео";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // shit1
+            // 
+            this.shit1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.shit1.Location = new System.Drawing.Point(530, 251);
+            this.shit1.Name = "shit1";
+            this.shit1.Size = new System.Drawing.Size(120, 22);
+            this.shit1.TabIndex = 6;
+            this.shit1.Tag = "double cannyThreshold";
+            this.shit1.ValueChanged += new System.EventHandler(this.shit1_ValueChanged);
+            // 
+            // shit2
+            // 
+            this.shit2.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.shit2.Location = new System.Drawing.Point(530, 324);
+            this.shit2.Name = "shit2";
+            this.shit2.Size = new System.Drawing.Size(120, 22);
+            this.shit2.TabIndex = 7;
+            this.shit2.Tag = "double cannyThresholdLinking";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1175, 450);
+            this.Controls.Add(this.shit2);
+            this.Controls.Add(this.shit1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Tag = "double cannyThreshold";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shit2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.NumericUpDown shit1;
+        private System.Windows.Forms.NumericUpDown shit2;
+    }
+}
+
---
+++ Чугунова_ровииас_1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace Чугунова_ровииас_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            capture = new VideoCapture();
+            
+        }
+        private Image<Bgr, byte> sourceImage;
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла
+
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = openFileDialog.FileName;
+                sourceImage = new Image<Bgr, byte>(fileName);
+
+            }
+            imageBox1.Image = sourceImage.Resize(300, 300, Inter.Linear);
+            double cannyThreshold = (int)shit1.Value;
+            double cannyThresholdLinking = (int)shit2.Value;
+            imageBox2.Image = Filter(sourceImage, cannyThreshold, cannyThresholdLinking).Resize(300, 300, Inter.Linear); 
+
+
+
+        }
+
+        private void imageBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private VideoCapture capture;
+        
+
+        private void Camera()
+        {
+            // инициализация веб-камеры
+            
+            capture.ImageGrabbed += ProcessFrame;
+
+            capture.Start();
+            var frame = capture.QueryFrame();
+            sourceImage = frame.ToImage<Bgr, byte>();
+            imageBox1.Image = sourceImage.Resize(300, 300, Inter.Linear);
+            double cannyThreshold = (double)shit1.Value;
+            double cannyThresholdLinking = (double)shit2.Value;
+            imageBox2.Image = Filter(sourceImage, cannyThreshold, cannyThresholdLinking).Resize(300, 300, Inter.Linear);
+            capture.Stop();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if( button2.Text == "остановить")
+            {
+                button2.Text = "Камера";
+                timer1.Enabled = false;
+            }
+            else
+            {
+                button2.Text = "Остановить";
+                timer1.Enabled = true;
+            }
+        }
+        private void InitializwTimer()
+        {
+
+            capture = new VideoCapture();
+            timer1.Interval = 100;
+            timer1.Tick += new EventHandler(timer1_Tick);
+
+            timer1.Enabled = true;
+
+            button2.Text = "Остановить";
+            button2.Click += new EventHandler(button2_Click);
+        }
+        // захват кадра из видеопотока
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            var frame = new Mat();
+            capture.Retrieve(frame); // получение текущего кадра
+            Image<Bgr, byte> sourceImage = frame.ToImage<Bgr, byte>();
+           
+
+        }
+        private void ProcessFrameVideo(object sender, EventArgs e)
+        {
+            var frame = new Mat();
+            capture.Retrieve(frame); // получение текущего кадра
+            var picture = frame.ToImage<Bgr, byte>();
+            var picture2 = frame.ToImage<Bgr, byte>();
+            imageBox1.Image = picture.Resize(640, 480, Inter.Linear);
+            double cannyThreshold = (int)shit1.Value;
+            double cannyThresholdLinking = (int)shit2.Value;
+            imageBox2.Image = Filter(picture2, cannyThreshold, cannyThresholdLinking).Resize(640, 480, Inter.Linear);
+            //imageBox1.Image = frame.ToImage<Bgr, byte>();
+            //imageBox2.Image = frame.ToImage<Gray,byte>();
+            //Thread.Sleep((int)capture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Video Files (*mp4, *.flv)| *.mp4;*.flv";
+            var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла
+            if (result == DialogResult.OK) // открытие выбранного файла
+            {
+                string fileName = openFileDialog.FileName;
+                capture = new VideoCapture(fileName);
+                capture.ImageGrabbed += ProcessFrameVideo;
+                capture.Start(); // начало обработки видеопотока
+                
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Camera();
+        }
+
+       
+
+        private static Image<Bgr, byte> Filter(Image<Bgr, byte> source, double Threshold, double ThresholdLinking)
+        {
+            
+            Image<Gray, byte> grayImage = source.Convert<Gray, byte>();
+            var tempImage = grayImage.PyrDown();// понижает качество
+            var destImage = tempImage.PyrUp();// повышает качество
+            
+           
+            Image<Gray, byte> cannyEdges = destImage.Canny(Threshold, ThresholdLinking);
+            var cannyEdgesBgr = cannyEdges.Convert<Bgr, byte>();
+            var resultImage = source.Sub(cannyEdgesBgr); // попиксельное вычитание
+                                                         //обход по каналам
+            for (int channel = 0; channel < resultImage.NumberOfChannels; channel++)
+                for (int x = 0; x < resultImage.Width; x++)
+                    for (int y = 0; y < resultImage.Height; y++) // обход по пискелям
+                    {
+                        // получение цвета пикселя
+                        byte color = resultImage.Data[y, x, channel];
+                        if (color <= 50)
+                            color = 0;
+                        else if (color <= 100)
+                            color = 25;
+                        else if (color <= 150)
+                            color = 180;
+                        else if (color <= 200)
+                            color = 210;
+                        else
+                            color = 255;
+                        resultImage.Data[y, x, channel] = color; // изменение цвета пикселя
+                    }
+            return resultImage;
+        }
+
+        private void shit1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
